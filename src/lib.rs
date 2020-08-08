f979575f9bac6a65a88d3f95d7518faa78cfa788
Cepa <cristian@MacBook-Pro-de-Cristian.local>
2020-08-08 17:54:50 -0300

    fix white center

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use wasm_bindgen::JsCast;
 //con web_sys podemos interactuar con las clases que manejan el DOM
 use web_sys::{CanvasRenderingContext2d};
 
-const MAX_ITER: i64 = 10000;
+const MAX_ITER: i32 = 1000;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -21,7 +21,7 @@ extern {
     fn alert(s: &str);
 
     //#[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn log_i64(a: i64);
+    fn log_i32(a: i32);
 }
 
 #[wasm_bindgen]
@@ -35,7 +35,7 @@ fn main(){
 
 #[wasm_bindgen]
 pub fn draw(ctx:&CanvasRenderingContext2d,
-    width: i64, height: i64, x1: i64, y1: i64, rss: f64, rse: f64, iss: f64, ise: f64){
+    width: i32, height: i32, x1: i32, y1: i32, rss: f64, rse: f64, iss: f64, ise: f64){
 
     for x in 0..width{
         for y in 0..height{
@@ -43,21 +43,21 @@ pub fn draw(ctx:&CanvasRenderingContext2d,
             let xi = x1 - (x1 - x);
             let yi = y1 - (y1 - y);
 
-            let mut mandelbrot_point = calculateSet(xi, yi, width, height, rss, rse, iss, ise);
+            let mandelbrot_point = calculateSet(xi, yi, width, height, rss, rse, iss, ise);
 
             if(mandelbrot_point == MAX_ITER){
-                mandelbrot_point = 0;
+                ctx.set_fill_style(&"#000".into());
+                ctx.fill_rect(x as f64,y as f64, 1.0, 1.0);
+            } else{
+                let str = format!("hsl(0,100%, {m}%)", m = mandelbrot_point);
+                ctx.set_fill_style(&str.into());
+                ctx.fill_rect(x as f64,y as f64, 1.0, 1.0);
             }
-
-            let str = format!("hsl(0,100%, {m}%)", m = mandelbrot_point);
-            ctx.set_fill_style(&str.into());
-            ctx.fill_rect(x as f64,y as f64, 1.0, 1.0);
-
         }
     }
 }
 
-fn calculateSet(x0: i64 , y0: i64, width: i64, height: i64, rss: f64, rse: f64, iss: f64, ise: f64) -> i64{
+fn calculateSet(x0: i32 , y0: i32, width: i32, height: i32, rss: f64, rse: f64, iss: f64, ise: f64) -> i32{
 
     let mut a = rss + ((x0 as f64 / width as f64) * (rse - rss));
     let mut b = iss + ((y0 as f64 / height as f64) * (ise - iss));
@@ -67,7 +67,7 @@ fn calculateSet(x0: i64 , y0: i64, width: i64, height: i64, rss: f64, rse: f64, 
 
     let mut i = 0;
 
-    for n in 0..MAX_ITER {
+    for n in 0..(MAX_ITER + 1) {
         let mut an = (a * a) - (b * b);
         let mut bn = 2.0 * a * b;
 
